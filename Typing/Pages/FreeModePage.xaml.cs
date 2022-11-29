9b98961ec8ec6910f0ddb19364903badae6fa78f
nezhadian <yasin.ebr.nezh@gmail.com>
2022-11-29 21:23:58 +0330

    Free Mode Page Added

---
+++ Typing/Pages/FreeModePage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Typing.Pages
+{
+    /// <summary>
+    /// Interaction logic for FreeModePage.xaml
+    /// </summary>
+    public partial class FreeModePage : Page
+    {
+        public FreeModePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
