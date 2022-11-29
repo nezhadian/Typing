0585cd0a18844bae555f36b98a602a54a4d6a787
nezhadian <yasin.ebr.nezh@gmail.com>
2022-11-29 20:32:42 +0330

    Revert "Fix Level Mode Page In Pages NameSpace"
    This reverts commit 7621a68acfbf53f5a4bb208dc4f3d816bc692f22.

--- Typing/Pages/LevelModePage.xaml.cs
+++ Typing/Pages/LevelModePage.xaml.cs
@@ -11,7 +11,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Typing
+namespace Typing.Pages
 {
     /// <summary>
     /// Interaction logic for LevelModePage.xaml
