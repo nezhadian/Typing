7621a68acfbf53f5a4bb208dc4f3d816bc692f22
nezhadian <yasin.ebr.nezh@gmail.com>
2022-11-29 20:30:30 +0330

    Fix Level Mode Page In Pages NameSpace

--- Typing/Pages/LevelModePage.xaml.cs
+++ Typing/Pages/LevelModePage.xaml.cs
@@ -11,7 +11,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Typing.Pages
+namespace Typing
 {
     /// <summary>
     /// Interaction logic for LevelModePage.xaml
