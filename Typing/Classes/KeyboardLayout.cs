9533e757aeeb1ed656e55226a9fc8579803dfc16
nezhadian <yasin.ebr.nezh@gmail.com>
2023-01-01 17:23:10 +0330

    Add OEM Period And 1-9 Keys in KeyboardLayout

--- Typing/Classes/KeyboardLayout.cs
+++ Typing/Classes/KeyboardLayout.cs
@@ -46,6 +46,19 @@ namespace Typing
             {Key.Oem2,('/','?') },
             {Key.Oem7,('\'','"') },
             {Key.OemComma,(',','<') },
+            {Key.OemPeriod,('.','>') },
+            
+            //1-9
+            {Key.D0,('0',')') },
+            {Key.D1,('1','!') },
+            {Key.D2,('2','@') },
+            {Key.D3,('3','#') },
+            {Key.D4,('4','$') },
+            {Key.D5,('5','%') },
+            {Key.D6,('6','^') },
+            {Key.D7,('7','&') },
+            {Key.D8,('8','*') },
+            {Key.D9,('9','(') },
         };
 
         public char? Key2Char(Key key, bool isToggled)
